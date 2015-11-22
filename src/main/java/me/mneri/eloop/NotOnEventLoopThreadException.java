dd81b6e7d98795ae31987fc0cad74b510c2ef13e
Massimo Neri <hello@mneri.me>
2015-11-22 23:12:26 +0100

    Added NotOnEventLoopThreadException

---
+++ src/main/java/me/mneri/eloop/NotOnEventLoopThreadException.java
@@ -0,0 +1,35 @@
+/*
+ * This file is part of eloop.
+ *
+ * eloop is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * eloop is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with eloop. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package me.mneri.eloop;
+
+/**
+ * This exception is thrown when a method restricted to the event loop thread is invoked from another thread.
+ *
+ * @author Massimo Neri
+ * @version 1.0
+ */
+public class NotOnEventLoopThreadException extends RuntimeException {
+    /**
+     * Create a new instance of this exception with the specified {@code methodSignature}.
+     *
+     * @param methodSignature A string representation of the method, used in exception's message.
+     */
+    public NotOnEventLoopThreadException(String methodSignature) {
+        super("Method " + methodSignature + " should be invoked from event loop thread.");
+    }
+}
